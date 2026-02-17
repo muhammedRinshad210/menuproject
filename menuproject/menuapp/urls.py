8a3071f56117b9fdd1efb5454a07047fa2101a1a
Rinshad2 <mr8144587@gmail.com>
2026-02-17 16:29:34 +0530

    admin

--- menuproject/menuapp/urls.py
+++ menuproject/menuapp/urls.py
@@ -1,6 +1,9 @@
-from django.urls import re_path
-from . import views  
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    re_path(r'^$',views.index,name="index")
+    path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('edit/<int:id>/', views.edit_carousel, name='edit_carousel'),
+    path('delete/<int:id>/', views.delete_carousel, name='delete_carousel'),
 ]
--- menuproject/menuapp/views.py
+++ menuproject/menuapp/views.py
@@ -3,3 +3,54 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "menuapp/index.html")
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Carousel
+from .forms import CarouselForm
+
+
+# Home Page
+def home(request):
+    carousels = Carousel.objects.all().order_by('-id')
+    return render(request, 'menuapp/index.html', {'carousels': carousels})
+
+
+# Dashboard Page
+def dashboard(request):
+    carousels = Carousel.objects.all().order_by('-id')
+    form = CarouselForm()
+
+    if request.method == 'POST':
+        form = CarouselForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+
+    return render(request, 'menuapp/admin/dashboard.html', {
+        'form': form,
+        'carousels': carousels
+    })
+
+
+# Edit Carousel
+def edit_carousel(request, id):
+    carousel = get_object_or_404(Carousel, id=id)
+    form = CarouselForm(instance=carousel)
+
+    if request.method == 'POST':
+        form = CarouselForm(request.POST, request.FILES, instance=carousel)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+
+    return render(request, 'menuapp/admin/dashboard.html', {
+        'form': form,
+        'carousels': Carousel.objects.all()
+    })
+
+
+# Delete Carousel
+def delete_carousel(request, id):
+    carousel = get_object_or_404(Carousel, id=id)
+    carousel.delete()
+    return redirect('dashboard')
